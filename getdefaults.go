8c20aac23c2632bef76a5cc3a9175d449f77885c
Marcel van Eck <vaneck.marcel@gmail.com>
2023-10-29 17:27:40 -0700

    Documentation added

--- getdefaults.go
+++ getdefaults.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// Boolean function returning whether the value of any type is in the list of options.
 func In(value interface{}, opt []interface{}) bool {
 	for _, i := range opt {
 		if i == value {
@@ -14,6 +15,7 @@ func In(value interface{}, opt []interface{}) bool {
 	return false
 }
 
+// Boolean function returning whether the string value is in the list of options.
 func InStr(value string, opt []string) bool {
 	for _, i := range opt {
 		if i == value {
@@ -23,6 +25,7 @@ func InStr(value string, opt []string) bool {
 	return false
 }
 
+// Return the string value if it is in the list of options (strings), otherwise return the default value.
 func ValInStrOrDefault(value string, default_value string, opt []string) string {
 	if InStr(value, opt) {
 		return value
@@ -30,6 +33,7 @@ func ValInStrOrDefault(value string, default_value string, opt []string) string 
 	return default_value
 }
 
+// Return the value if it is in the list of options (any type), otherwise return the default value.
 func ValInOrDefault(value interface{}, default_value interface{}, opt []interface{}) interface{} {
 	if In(value, opt) {
 		return value
@@ -37,6 +41,7 @@ func ValInOrDefault(value interface{}, default_value interface{}, opt []interfac
 	return default_value
 }
 
+// Return the value if it is not in the list of options (any type), otherwise return the default value.
 func ValNotInOrDefault(value interface{}, default_value interface{}, opt []interface{}) interface{} {
 	if In(value, opt) {
 		return default_value
@@ -44,6 +49,10 @@ func ValNotInOrDefault(value interface{}, default_value interface{}, opt []inter
 	return value
 }
 
+// Return the boolean value if the value represents a boolean value, otherwise return the default boolean value.
+// The value is considered a boolean value if it is a string containing one of the following values (any type):
+// true, t, yes, y, != 0
+// false, f, no, n, 0
 func BoolOrDefault(value interface{}, default_value bool) bool {
 	switch v := value.(type) {
 	case string:
